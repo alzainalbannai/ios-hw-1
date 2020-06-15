3a3b7824034a1fcf4d2268933b08f62116cb1274
alzainalbannai <alzainalbannai@users.noreply.github.com>
2020-06-15 16:58:46 +0300

    done 1 ,2 & 3
    easy

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -5,12 +5,12 @@
  
  */
 
-let name = "Ahmad"
+var name = "Ahmad"
 
 name = "Salem"
 
 
-var myWeight = 60
+var myWeight = 60.5
 var FriendWeight = 80.5
 var height = 1.5
 
@@ -38,28 +38,38 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "alzain"
+var lastName1 = "albannai"
+var email1 = "alzainalbannai@gmail.com"
+var phoneNumber1 = "96569969958"
+var age1 = 17
+var country1 = "kuwait"
+var area1 = "bayan"
+var password1 = "1234"
+var isKuwaiti1 = true
 
 //Person 2
-
-
-
-
-
-
-
-
+var firstName2 = "manar"
+var lastName2 = "albannai"
+var email2 = "manaralbannai@gmail.com"
+var phoneNumber2 = "96599811262"
+var age2 = 15
+var country2 = "kuwait"
+var area2 = "bayan"
+var password2 = "1233"
+var isKuwaiti2 = true
 
 
 //Person 3
+var firstName3 = "batool"
+var lastName3 = "hussain"
+var email3 = "batoolali@gmail.com"
+var phoneNumber3 = "96551297044"
+var age3 = 17
+var country3 = "kuwait"
+var area3 = "bayan"
+var password3 = "12333"
+var isKuwaiti3 = true
 
 
 
@@ -82,3 +92,40 @@ var isKuwaiti = false
 
  
  */
+firstName1 == firstName2 || firstName2 == firstName3 || firstName3 == firstName1
+firstName1 != firstName2 || firstName2 != firstName3 || firstName3 != firstName1
+
+lastName1 == lastName2 || lastName2 == lastName3 || lastName3 == lastName1
+lastName1 != lastName2 || lastName2 != lastName3 || lastName3 != lastName1
+
+email1 == email2 || email2 == email3 || email3 == email1
+email1 != email2 || email2 != email3 || email3 != email1
+
+phoneNumber1 == phoneNumber2 || phoneNumber2 == phoneNumber3 || phoneNumber3 == phoneNumber1
+phoneNumber1 != phoneNumber2 || phoneNumber2 != phoneNumber3 || phoneNumber3 != phoneNumber1
+
+age1 == age2 || age2 == age3 || age3 == age1
+age1 != age2 || age2 != age3 || age3 != age1
+age1 < age2 || age2 < age3 || age3 < age1
+age1 > age2 || age2 > age3 || age3 > age1
+age1 <= age2 || age2 <= age3 || age3 <= age1
+age1 >= age2 || age2 >= age3 || age3 >= age1
+
+country1 == country2 || country2 == country3 || country3 == country1
+country1 != country2 || country2 != country3 || country3 != country1
+
+area1 == area2 || area2 == area3 || area3 == area1
+area1 != area2 || area2 != area3 || area3 != area1
+
+password1 == password2 || password2 == password3 || password3 == password1
+password1 != password2 || password2 != password3 || password3 != password1
+
+isKuwaiti1 == isKuwaiti2 || isKuwaiti2 == isKuwaiti3 || isKuwaiti3 == isKuwaiti1
+isKuwaiti1 != isKuwaiti2 || isKuwaiti2 != isKuwaiti3 || isKuwaiti3 != isKuwaiti1
+
+age1 > 18 || age2 > 18 || age3 > 18
+age1 < 18 || age2 < 18 || age3 < 18
+
+age1 == 17 || age2 == 17 || age3 == 17
+age1 != 17 || age2 != 17 || age3 != 17
+
